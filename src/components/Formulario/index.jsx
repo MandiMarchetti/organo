d6987385e569a7d92a8360934ac2b8284411d0a7
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-08-27 12:47:32 +0200

    "Removed font-weight from CampoTexto CSS, added ListaSuspensa and Botao components to Formulario"

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,7 +1,10 @@
 import "./formulario.css";
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+import Botao from "../Botao";
 
 const Formulario = () => {
+  const times = ["Programação", "Front-end", "Data Science", "Devops", "UX e Design", "Mobile", "Inovação e Gestão"];
   return (
     <section className="formulario">
       <form>
@@ -9,6 +12,8 @@ const Formulario = () => {
         <CampoTexto label="Nome" placeholder="Digite seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={times} />
+        <Botao texto="Criar Card" />
       </form>
     </section>
   );
