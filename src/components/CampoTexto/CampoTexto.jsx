9fd5b4483a997a1645d0e620ac0ca12184e7990d
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-08-29 12:59:37 +0200

    "Added state and event handling to CampoTexto component, updated Formulario component to handle form submission, and added required attribute to CampoTexto and ListaSuspensa components."

--- src/components/CampoTexto/CampoTexto.jsx
+++ src/components/CampoTexto/CampoTexto.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import "./campoTexto.css";
 
 export const CampoTexto = (props) => {
+  // let valor = "";
+
+  const [valor, setValor] = useState("Amanda Thomaz");
+
+  const aoDigitado = (evento) => {
+    setValor(evento.target.value);
+    console.log(valor);
+  };
+
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input type="text" name="nome" placeholder={props.placeholder} />
+      <input
+        value={valor}
+        onChange={aoDigitado}
+        type="text"
+        name="nome"
+        placeholder={props.placeholder}
+        required={props.obrigatorio}
+      />
     </div>
   );
 };
--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -5,14 +5,20 @@ import Botao from "../Botao";
 
 const Formulario = () => {
   const times = ["Programação", "Front-end", "Data Science", "Devops", "UX e Design", "Mobile", "Inovação e Gestão"];
+
+  const aoCriarCard = (evento) => {
+    evento.preventDefault();
+    console.log("Form enviado!");
+  };
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoCriarCard}>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
-        <ListaSuspensa label="Time" itens={times} />
+        <CampoTexto label="Nome" placeholder="Digite seu nome" obrigatorio={true} />
+        <CampoTexto label="Cargo" placeholder="Digite seu cargo" obrigatorio={true} />
+        <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" obrigatorio={true} />
+        <ListaSuspensa label="Time" itens={times} obrigatorio={true} />
         <Botao>Criar Card</Botao>
       </form>
     </section>
